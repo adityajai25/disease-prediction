9e6d7ac59c227c5b5857930e9e3b57664b98c376
adityajai25 <adityajai243@gmail.com>
2023-11-14 16:46:26 +0530

    mongo setup

--- app.py
+++ app.py
@@ -7,7 +7,19 @@ import bz2
 import datetime
 import numpy as np
 import pandas as pd
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
 
+
+uri = "mongodb+srv://adityajai243:db2023@cluster0.buez9p5.mongodb.net/?retryWrites=true&w=majority"
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'))
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
 
 app = Flask(__name__)
 CORS(app)
